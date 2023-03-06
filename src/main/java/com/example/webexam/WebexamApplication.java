8cf27de24f12e815443130ffd509ed6c29a63bc0
ymkim97 <rladudaud97@naver.com>
2023-03-06 16:07:03 +0900

    initiate spring mvc study

---
+++ src/main/java/com/example/webexam/WebexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.webexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebexamApplication.class, args);
+    }
+
+}
